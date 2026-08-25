2295ccc095dbf04ffb76a6e140c762445770740f
Aylan Dante <facuaylan20@gmail.com>
2026-08-24 22:05:13 -0300

    segundo cambio

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -1,2 +1,9 @@
-tiempo = (4)
-probando = str("Tiempo")
+import math
+import time
+
+def menu():
+    print("1) Ingresar datos del niño")
+    print("2) Calcular promedio del tiempo en pantalla (mins)")
+    print("3) Dispositivo mas utilizado")
+    print("4) Exposicion")
+    eleccion = int(input("Elija una opcion!"))
