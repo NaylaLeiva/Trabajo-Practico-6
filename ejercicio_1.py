ac93f052b419c76aacf7876bfb36d926f2b876d0
Aylan Dante <facuaylan20@gmail.com>
2026-08-25 16:53:21 -0300

    Esqueleto de Mini Menu 'Consola'

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -2,8 +2,22 @@ import math
 import time
 
 def menu():
+    print("--------------------------------------------------")
     print("1) Ingresar datos del niño")
     print("2) Calcular promedio del tiempo en pantalla (mins)")
     print("3) Dispositivo mas utilizado")
     print("4) Exposicion")
-    eleccion = int(input("Elija una opcion!"))
+    print("5) Salir")
+    print("--------------------------------------------------")
+    eleccion = int(input("Elija una opcion! --> "))
+
+    while not (1 <= eleccion <= 5):
+        eleccion = int(input("Opcion invalida, intenta otra vez: "))
+    
+    return eleccion
+
+def main():
+    opcion = menu()
+    print("Elegiste la opcion: ", opcion)
+
+main()
